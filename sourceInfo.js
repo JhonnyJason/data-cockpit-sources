d88be6789c89fd978b82bb006ddd5b827482fdbf
Lenard Frommelt <lenny09918050@googlemail.com>
2021-06-04 09:42:41 +0200

    1

--- sourceInfo.js
+++ sourceInfo.js
@@ -1,7 +1,10 @@
 module.exports = {
     getDependencies: () => {
         return {
-            "codejar": "^3.4.0"
+            "codejar": "^3.4.0",
+            "qr-scanner": "^1.2.0",
+            "secret-manager-client": "^0.0.2",
+            "vanillaqr": "^1.0.0" 
         }
     }
 }
